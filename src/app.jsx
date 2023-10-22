051ddeb520f6f3d3241e4aa1ed731a6c326b50da
Bayu Maulana <bayufransdo@gmail.com>
2023-10-23 00:52:27 +0700

    23 oct 2023

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,40 @@
+const books = [
+  {
+    title: 'first book',
+    author: 'babayo',
+    id: 1,
+  },
+  {
+    title: 'second book',
+    author: 'babaji',
+    id: 2,
+  },
+];
+
+const Book = ({ id, title, author }) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#">{id}</a>
+        </li>
+        <li>
+          <a href="#">{title}</a>
+        </li>
+        <li>
+          <a href="#">{author}</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      {books.map((book) => {
+        return <Book {...book} asd="234" key={book.id} />;
+      })}
     </div>
   );
 };
