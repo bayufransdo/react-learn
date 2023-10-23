8952b4223cd4244d7f1d4893a709f868613c8d77
Bayu Maulana <bayufransdo@gmail.com>
2023-10-23 21:50:02 +0700

    start learn react hooks

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,9 @@
-const books = [
-  {
-    title: 'first book',
-    author: 'babayo',
-    id: 1,
-  },
-  {
-    title: 'second book',
-    author: 'babaji',
-    id: 2,
-  },
-];
-
-const Book = ({ title, author }) => {
-  const getBook = () => {
-    const book = books.find((book) => book.title === title);
-    console.log(book);
-  };
+function App() {
   return (
-    <div style={{ marginBottom: '20px', backgroundColor: '#333333' }}>
-      <p>{title}</p>
-      <p>{author}</p>
-      <button onClick={getBook}>Klik me</button>
+    <div className='container'>
+      <h2>Advanced React</h2>
     </div>
   );
-};
+}
 
-const App = () => {
-  return (
-    <div>
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
-      })}
-    </div>
-  );
-};
 export default App;
